f21129c24fd624e89953347870d9faba03ceaed4
Evan Lloyd New-Schmidt <evan@new-schmidt.com>
2023-02-28 07:54:07 -0500

    Always use current hash for version

--- build.rs
+++ build.rs
@@ -4,13 +4,20 @@ use std::process::Command;
 ///
 /// NOTE: Cargo.toml still needs to be updated on releases
 fn set_version_from_git() {
-    let cmd = Command::new("git").arg("describe").arg("--dirty").output();
+    let cmd = Command::new("git")
+        .arg("describe")
+        .arg("--always")
+        .arg("--dirty")
+        .output();
 
     match cmd {
         Ok(output) if output.status.success() => {
             let version = String::from_utf8_lossy(&output.stdout);
             let version = version.trim();
             println!("cargo:rustc-env=CARGO_GIT_VERSION={}", version);
+            // rerun when git checks out another ref or any ref changes
+            println!("cargo:rerun-if-changed=.git/refs/");
+            println!("cargo:rerun-if-changed=.git/HEAD");
         }
         _ => {
             // crates.io builds without git, so ignore here
